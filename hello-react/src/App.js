7124db5f64448b2ebd1b00b149ee1f3bd4b7dfc3
Doreen162 <doreenligman17@gmail.com>
2021-03-16 10:42:05 +0200

    Adding styling

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,28 +1,13 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const link = "http://www.google.com";
-
   return (
     <div className="App">
-      {/* Components */}
+      {/* other way <Navbar></Navbar> */}
+      <Navbar />
       <div className="content">
-         <h1>App Component</h1>
-         {/* Returns word in const title */}
-         <h1>{ title }</h1>
-         {/* Returns word in const likes & number 50 */}
-         <p>Liked { likes } times </p>
-
-         {/* returns whats in brackets */}
-         <p>{10}</p>
-         <p>{"hello ninjas"}</p>
-         <p> {[1,2,3,4,5]}</p>
-         <p>{ Math.random() * 10}</p>
-
-         {/* Returns word in const link */}
-         <a href={link}>Google Site</a>
+        <Home />
          </div>
     </div>
   );

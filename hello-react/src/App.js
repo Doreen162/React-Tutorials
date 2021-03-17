03bf82c665b20a98e47cba0e7083a3b72c739073
Doreen162 <doreenligman17@gmail.com>
2021-03-17 15:39:59 +0200

    404 Pages & Next Steps

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,31 +1,32 @@
 import Navbar from './Navbar';
 import Home from './Home';
-//install react-router-dom@5 & import React Router
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      {/* other way <Navbar></Navbar> */}
-      <Navbar />
-      <div className="content">
-        <Switch>
-          {/* path to Homepage */}
-          <Route exact path ="/"> 
-             <Home />
-          </Route>
-          <Route path="/create"> 
-             <Create />
-          </Route>
-          <Route path="/blogs/:id"> 
-             <BlogDetails />
-          </Route>
-        </Switch>
-         </div>
-    </div>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </div>
     </Router>
   );
 }

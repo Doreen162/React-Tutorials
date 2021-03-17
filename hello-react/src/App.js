ecda65362ae644cf481a3f8158bd9e567bdf8efd
Doreen162 <doreenligman17@gmail.com>
2021-03-17 13:09:26 +0200

    React Router

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,15 +1,24 @@
 import Navbar from './Navbar';
 import Home from './Home';
+//install react-router-dom@5 & import React Router
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       {/* other way <Navbar></Navbar> */}
       <Navbar />
       <div className="content">
-        <Home />
+        <Switch>
+          {/* path to Homepage */}
+          <Route path="/"> 
+             <Home/>
+          </Route>
+        </Switch>
          </div>
     </div>
+    </Router>
   );
 }
 
